41914f21bd58c22a80718083c4e27d0f647fba6b
hhoangg <hoangxuan2402@gmail.com>
2026-07-30 21:32:37 +0700

    zptah 1.0.0

--- Casks/zptah.rb
+++ Casks/zptah.rb
@@ -1,6 +1,6 @@
 cask "zptah" do
-  version "0.29.0"
-  sha256  "731f96aeacb322986e2d122e7d7932cd9f9e599da25fbfaadc5b43c49200cb8d"
+  version "1.0.0"
+  sha256  "67082f4369c275e69cfe46b7385a9ae0d48426ba06b01d249d038b2ff75a3602"
 
   url "https://github.com/hhoangg/homebrew-zptah/releases/download/v#{version}/zptah.app.zip"
   name "zptah"
